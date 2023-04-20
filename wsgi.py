4534e9eadd258279fc948cb9010465548c272362
Faith <127827447+Remii1@users.noreply.github.com>
2023-04-20 16:19:13 +0300

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for CrudProjectKarimi1 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CrudProjectKarimi1.settings')
+
+application = get_wsgi_application()
